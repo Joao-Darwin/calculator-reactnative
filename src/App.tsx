3da5fad2736619ec209bb0baf97b265422a868d9
João Darwin <joaodarwin.ip22@gmail.com>
2024-03-29 00:54:43 -0300

    feat: Organizar componentes de botões no app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
-  SafeAreaView, Text
+  SafeAreaView, StyleSheet, View
 } from 'react-native';
 import Button from './components/Button';
 
+const style = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  }
+})
+
 function App(): React.JSX.Element {
 
-  const [number, setNumber] = useState(0);
-
   return (
-    <SafeAreaView>
-      <Text>{number}</Text>
-      <Button title='Click here' onClick={() => setNumber(number + 1)} />
+    <SafeAreaView style={style.container}>
+      <View style={style.buttons}>
+        <Button title='AC' />
+        <Button title='/' />
+        <Button title='7' />
+        <Button title='8' />
+        <Button title='9' />
+        <Button title='*' />
+        <Button title='4' />
+        <Button title='5' />
+        <Button title='6' />
+        <Button title='-' />
+        <Button title='1' />
+        <Button title='2' />
+        <Button title='3' />
+        <Button title='+' />
+        <Button title='0' />
+        <Button title='.' />
+        <Button title='=' />
+      </View>
     </SafeAreaView>
   );
 }
